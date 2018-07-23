25c1f789ee8d566e184a39e64563c2611a676ea6
JheeBz <christian_kirkeby@hotmail.com>
2018-07-23 21:39:23 +1000

    Added further styling to the Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,19 +12,26 @@ export default props => {
 export const CardButton = styled.button`
   font-family: 'Lato', sans-serif;
   box-sizing: inherit;
-  flex: 1;
+  flex: ${props => props.large ? 5 : 1};
   display: block;
   border: 2px solid ${props => props.theme.primary};
   background: ${props => props.primary ? props.theme.primary : props.theme.white};
   color: ${props => props.primary ? props.theme.white : props.theme.primary};
-  font-size: ${props => props.large ? '1.25rem' : '1rem'};
-  margin:  ${props => props.large ? '1.25rem' : '1rem'};
-  padding: ${props => props.large ? '0.50rem 1.25rem' : '0.25rem 1rem'};
+  font-size: 1.25rem;
+  margin:  1.25rem;
+  padding: 0.50rem 1.25rem;
   border-radius: 3px;
+  cursor: pointer;
+  :hover {
+    background: ${props => props.primary ? props.theme.white : props.theme.primary};
+    color: ${props => props.primary ? props.theme.primary : props.theme.white};
+    transition: 0.2s ease-out;
+  }
 `
 
 export const ButtonList = styled.div`
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-between;
 `
 
@@ -32,7 +39,7 @@ const Card = styled.div`
   padding: 48px;
   display: flex;
   flex-direction: column;
-  min-width: 400px;
+  min-width: 250px;
   max-width: 500px;
   min-height: 100px;
   box-shadow: ${props => props.theme.boxShadow};
@@ -46,7 +53,7 @@ export const CardTitle = styled.h2`
   font-size: 2rem;
 `
 
-export const CardBody = styled.p`
+export const CardBody = styled.div`
   font-size: 1rem;
-  font-family: 'Lato';
+  font-family: 'Lato', sans-serif;
 `
