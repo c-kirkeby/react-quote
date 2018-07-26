47701cd01cb4fad168bd1cdd00b3c3ca57595143
JheeBz <christian_kirkeby@hotmail.com>
2018-07-27 07:18:23 +1000

    Made buttons more responsive on smaller screens

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,13 +3,12 @@ import styled from 'styled-components'
 const Button = styled.button`
   font-family: 'Lato', sans-serif;
   box-sizing: inherit;
-  flex: ${props => props.large ? 5 : 1};
-  display: block;
+  flex: ${props => props.large ? '2' : '1'};
   border: 2px solid ${props => props.theme.primary};
   background: ${props => props.primary ? props.theme.primary : props.theme.white};
   color: ${props => props.primary ? props.theme.white : props.theme.primary};
   font-size: 1.25rem;
-  margin:  1.25rem;
+  margin:  0.25rem;
   padding: 0.50rem 1.25rem;
   border-radius: 3px;
   cursor: pointer;
--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -4,6 +4,10 @@ const ButtonList = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
+
+  @media screen and (max-width: 400px) {
+    flex-direction: column;
+  }
 `
 
 export default ButtonList
