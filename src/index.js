e7b6ffeedfe65effbc4c96beabaea0387b13abee
JheeBz <christian_kirkeby@hotmail.com>
2018-08-04 08:36:23 +1000

    Added loading component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Loadable from 'react-loadable'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = Loadable({
+  loader: () => import('./components/App'),
+  loading() {
+    return <div></div>
+  }
+})
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
