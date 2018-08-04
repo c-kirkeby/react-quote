1757e7d1919eb56894f55783b3e60fdc338b471f
JheeBz <christian_kirkeby@hotmail.com>
2018-08-04 10:26:08 +1000

    Changed the backgrounds fetched from unsplash

--- src/components/Background.js
+++ src/components/Background.js
@@ -6,6 +6,6 @@ export default styled.div`
       rgba(42,43,57, 0.70),
       rgba(42,43,57, 0.70)
     ),
-    url('https://source.unsplash.com/1920x1080/?mountain,lake') no-repeat center center fixed;
+    url('https://source.unsplash.com/1920x1080/?mountain+snow+lake') no-repeat center center fixed;
   background-size: cover;
 `
